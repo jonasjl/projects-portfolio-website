559081db4acdb784c45483ca9334ea47e3a5982d
jonasjl <59828145+jonasjl@users.noreply.github.com>
2024-07-30 09:42:05 +0200

    structure change + navbar compontent

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,15 @@
+import "./Navbar.css";
+
+export const Navbar = () => {
+  return (
+    <div className="navbar">
+      <ul className="nav-menu">
+        <li>Home</li>
+        <li>About me</li>
+        <li>Services</li>
+        <li>Portfolio</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
+};
