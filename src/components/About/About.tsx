7b1091826d12ed6be7d8b24b7a828d783651357c
Jonas <Jonas.lillehovde@statnett.no>
2024-08-08 14:43:09 +0200

    sortabletable done

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Header } from "../Header/Header";
 import { SortableTable } from "../Sortingtable/SortableTable";
+import data from "../Sortingtable/data.json"
 
 export const About: React.FC = () => {
   return (
     <div>
       <Header title="About Us" text="Learn more about our company and team"/>
-      <SortableTable/>
+      <SortableTable data={data}/> 
     </div>
   );
 };
